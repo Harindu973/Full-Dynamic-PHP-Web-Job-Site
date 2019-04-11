b2f110303ff48a8588f7f16edab04f19fcfaee50
Harindu Lakshan <42797678+Harindu973@users.noreply.github.com>
2019-04-11 22:58:30 +0530

    Database Connection

---
+++ dbcon.php
@@ -0,0 +1,14 @@
+<?php
+
+$server="localhost";
+$uname="root";
+$pw="";
+$dbname="jobseeker";
+
+$conn= mysqli_connect($server,$uname,$pw,$dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: ");
+    
+} 
+?>
